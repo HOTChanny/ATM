d266138810849c13459eca9d87da960da8ecdf6f
김하챤 <dbtldk12@naver.com>
2024-02-07 14:21:55 +0900

    로그인임시추가

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+    //로그인 화면 Root 
+    public GameObject LoginView;
+
+    public InputField inputField_ID;
+    public InputField inputField_PW;
+    public Button Button_Login;
+
+    //Test를 위해 임의로 사용자 변수를 추가
+    private string user = "User";
+    private string password = "1234";
+
+    /// 
+    /// 로그인 버튼 클릭시 실행
+    /// 
+    public void LoginButtonClick()
+    {
+        if (inputField_ID.text == user && inputField_PW.text == password)
+        {
+            Debug.Log("로그인 성공");
+            SceneManager.LoadScene("ATMMainScene");
+        }
+        else
+        {
+            Debug.Log("로그인 실패");
+        }
+    }
+}
