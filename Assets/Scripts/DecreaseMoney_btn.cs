276ec4679eeea770b180f76cb795d872732c63ac
김하챤 <dbtldk12@naver.com>
2024-02-07 20:58:04 +0900

    출금버튼
    뭐가..문제일까,,,

--- Assets/Scripts/DecreaseMoney_btn.cs
+++ Assets/Scripts/DecreaseMoney_btn.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class IncreaseMoney_btn : MonoBehaviour
+public class DecreasecreaseMoney_btn : MonoBehaviour
 {
     //입금_만원 버튼 클릭시, 잔액+, 현금-
 
-    public Text balanceText; // 계좌금액
-    public Text cashText; // 현금
+    public Text balanceText;
+    public Text cashText;
 
     int balance;
     int cash;
@@ -30,10 +30,10 @@ public class IncreaseMoney_btn : MonoBehaviour
     public void OnClick10()
     {
 
-        if (cash > balance)
+        if (cash < balance)
         {
-            balance = balance + 10000;
-            cash = cash - 10000;
+            balance = balance - 10000;
+            cash = cash + 10000;
         }
         else
         {
@@ -41,29 +41,29 @@ public class IncreaseMoney_btn : MonoBehaviour
         }
     }
 
-        public void OnClick30()
-        {
-         
+    public void OnClick30()
+    {
 
-            if (cash > balance)
-            {
-                balance = balance + 30000;
-                cash = cash - 30000;
-            }
-            else
-            {
-                Debug.Log("잔액 부족");
-            }
+
+        if (cash < balance)
+        {
+            balance = balance - 30000;
+            cash = cash + 30000;
         }
+        else
+        {
+            Debug.Log("잔액 부족");
+        }
+    }
 
     public void OnClick50()
     {
 
 
-        if (cash > balance)
+        if (cash < balance)
         {
-            balance = balance + 50000;
-            cash = cash - 50000;
+            balance = balance - 50000;
+            cash = cash + 50000;
         }
         else
         {
